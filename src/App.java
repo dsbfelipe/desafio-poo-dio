7914eef472f43a3e0cd828f7d2aa3d416df2b565
Felipe dos Santos Bento <dsbfelipe@outlook.com>
2025-07-22 18:20:50 -0300

    feat: add 'mentoria' and 'curso' classes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso javascript");
+        curso2.setDescricao("descrição curso javascript");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
